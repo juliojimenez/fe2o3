1909b9927c82b5501621d291c3dc7e15822a09a3
Julio Jimenez <julio@julioj.com>
2024-03-21 21:34:57 -0400

    ok
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,30 @@
 fn main() {
     println!("Hello, world!");
-    another_function();
+    another_function(5);
+    print_labeled_measurement(5, 'm');
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {y}");
+    let x = five();
+    println!("The value of x is: {x}");
+    let z = plus_one(5);
+    println!("The value of z is: {z}");
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
